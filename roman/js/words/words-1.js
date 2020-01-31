123cb55409a976364f61490169f969c3460702bc
Deny1741 <40074768+Deny1741@users.noreply.github.com>
2020-01-31 12:24:42 +0500

    Add files via upload

--- roman/js/words/words-1.js
+++ roman/js/words/words-1.js
@@ -1,11 +1,7 @@
 export let wordsRus1 = ['колесо', 'Мотор', 'пот', 'гусеница', 'хвост', 'Сантехник', 'попугай',
     'уголь', 'ствол дерева', 'корзина', 'куст', 'коготь', 'пламя', 'кулак', 'дупло', 'посылка', 'кирпич', 'чайник', 'ведро',
-    'камень', 'замок', 'треугольник', 'провод', 'парик', 'мыло', 'закат', 'созвездие', 'мел', 'кошелёк', 'грабитель',
-    'енот', 'корона', 'язык', 'коса из волос', 'забастовка', 'тренер', 'библиотека',
-    'борода', 'невеста', 'засуха'];
+    'камень'];
 
 export let wordsEng1 = ['wheel', 'engine', 'sweat', 'caterpillar', 'tail', 'plumber', 'parrot',
     'coal', 'trunk', 'basket', 'bush', 'claw', 'flame', 'fist', 'hollow', 'parcel', 'brick', 'kettle', 'bucket',
-    'stone', 'castle', 'triangle', 'wire', 'wig', 'soap', 'sunset', 'constellation', 'chalk', 'purse', 'robber',
-    'raccoon', 'crown', 'tongue', 'plait', 'strike', 'coach', 'library',
-    'beard', 'fiancee', 'drought'];
+    'stone'];
--- roman/js/words/words-2.js
+++ roman/js/words/words-2.js
@@ -1,3 +1,7 @@
-export let wordsRus2 = ['подушка'];
+export let wordsRus2 = ['замок', 'треугольник', 'провод', 'парик', 'мыло', 'закат', 'созвездие', 'мел', 'кошелёк', 'грабитель',
+    'енот', 'корона', 'язык', 'коса из волос', 'забастовка', 'тренер', 'библиотека',
+    'борода', 'невеста', 'засуха'];
 
-export let wordsEng2 = ['pillow'];
+export let wordsEng2 = ['castle', 'triangle', 'wire', 'wig', 'soap', 'sunset', 'constellation', 'chalk', 'purse', 'robber',
+    'raccoon', 'crown', 'tongue', 'plait', 'strike', 'coach', 'library',
+    'beard', 'fiancee', 'drought'];
